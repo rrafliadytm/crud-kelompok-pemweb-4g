dcebffe092f44c8d4cc6a63d0d275025dad5c0f3
rrafliadytm <rafliadyatma17@gmail.com>
2025-06-02 16:19:12 +0700

    update_login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id_pengguna();
+            $table->id('id_pengguna');
             $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
