cc0c11e7b4c13e805d65cb38eb1a2956106b548a
Nurfitriarp <93233737+Nurfitriarp@users.noreply.github.com>
2025-06-02 15:54:56 +0700

    db_update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id_pengguna();
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('no_telp')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2025_06_02_085356_create_pemesanans_table.php
+++ database/migrations/2025_06_02_085356_create_pemesanans_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tiket', function (Blueprint $table) {
+        Schema::create('pemesanans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tiket');
+        Schema::dropIfExists('pemesanans');
     }
 };
