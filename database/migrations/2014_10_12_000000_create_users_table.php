0ab9711098c66204a1d9a73a57b24be472b25039
adeliorhn <adeliorhna01@gmail.com>
2025-06-04 13:41:30 +0700

    betulin_db_user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id_pengguna');
-            $table->string('nama');
+            $table->id('id');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
