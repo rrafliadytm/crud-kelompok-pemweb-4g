ccd2fe65768e096bcb4b00b6bf517697f903d126
Nurfitriarp <93233737+Nurfitriarp@users.noreply.github.com>
2025-06-08 21:29:16 +0700

    update_homepage_dan_navbar_dikit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\pemesanan;
+use App\Models\penerbangan;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $penerbangan = penerbangan::all();
+        return view('home', compact('penerbangan'));
     }
 }
