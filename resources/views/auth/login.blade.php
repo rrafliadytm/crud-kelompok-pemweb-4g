47ee64f8160fb0924e16f430de7b294056f181b1
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-06-09 13:14:37 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                 <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Alamat E-Mail') }}</label>
 
                 <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="user@gmail.com">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail Address" style="border-radius: 50px; height:60px">
 
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -24,7 +24,7 @@
                 <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                 <div class="col-md-6">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="********">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password" style="border-radius: 50px; height:60px">
 
                     @error('password')
                         <span class="invalid-feedback" role="alert">
