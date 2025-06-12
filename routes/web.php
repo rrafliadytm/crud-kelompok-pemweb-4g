055be8d5a87eed3c5bf8821e937d31b3094eadb3
rrafliadytm <rafliadyatma17@gmail.com>
2025-06-12 11:44:04 +0700

    docker_config

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/about', [App\Http\Controllers\AboutController::class,'index'])->name('about');
