68e55aed240cc81da33260c46f0fb8456edb70f8
rrafliadytm <rafliadyatma17@gmail.com>
2025-06-15 12:35:45 +0700

    update_login_form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.page_login')
 
 @section('content')
-<div class="container">
+<div class="container d-flex justify-content-center align-items-center" style="height: 75vh;">
     <div class="card-body">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
-            <div class="row mb-3">
-                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Alamat E-Mail') }}</label>
-
+            
+            <div class="row mb-5 justify-content-center">
                 <div class="col-md-6">
+                    <label for="email" class="md-2 mb-3">{{ __('Alamat E-Mail') }}</label>
                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="user@email.com" style="border-radius: 50px; height:60px">
 
                     @error('email')
@@ -20,12 +19,21 @@
                 </div>
             </div>
 
-            <div class="row mb-3">
-                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
+            <div class="row mb-5 justify-content-center">                
                 <div class="col-md-6">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="********" style="border-radius: 50px; height:60px">
-
+                    <label for="password" class="md-2 mb-3">{{ __('Password') }}</label>
+                    <div class="position-relative">
+                        <input id="password" type="password" 
+                            class="form-control @error('password') is-invalid @enderror" 
+                            name="password" required autocomplete="current-password" 
+                            placeholder="********" 
+                            style="border-radius: 50px; height:60px; padding-right: 40px;">
+                        <span class="position-absolute top-50 end-0 translate-middle-y pe-3" 
+                            style="cursor: pointer;" 
+                            onclick="togglePassword()">
+                            <i class="fas fa-eye-slash" id="togglePasswordIcon" style="color: rgb(195, 195, 195)"></i>
+                        </span>
+                    </div>
                     @error('password')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -35,7 +43,7 @@
             </div>
 
            <div class="row mb-3">
-                <div class="col-md-6 offset-md-4">
+                <div class="col-md-6 offset-md-3">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="form-check m-0">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -47,14 +55,42 @@
                     </div>
                 </div>
             </div>
-            <div class="row mb-0">
-                <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary" style="border-radius: 50px; height:60px; width: 100px;">
+            <div class="row mb-3">
+                <div class="col-md-6 offset-md-3">
+                    <button type="submit" class="btn btn-primary w-100 w-md-auto" style="border-radius: 50px; height:60px; width: 100px;">
                         {{ __('Masuk') }}
                     </button>
                 </div>
             </div>
+            
+            @if (Route::has('register'))
+                <div class="row mb-3">
+                    <div class="col-md-6 offset-md-3">
+                        <button class="btn btn-outline-primary w-100 w-md-auto" style="border-radius: 50px; height:60px; width: 100px;" href="{{ route('register') }}">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Daftar') }}</a>
+                        </button>
+                    </div>
+                </div>
+            @endif
         </form>
     </div>
+    @section('scripts')
+    <script>
+    function togglePassword() {
+        const passwordInput = document.getElementById('password');
+        const toggleIcon = document.getElementById('togglePasswordIcon');
+        
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            toggleIcon.classList.remove('fa-eye-slash');
+            toggleIcon.classList.add('fa-eye');
+        } else {
+            passwordInput.type = 'password';
+            toggleIcon.classList.remove('fa-eye');
+            toggleIcon.classList.add('fa-eye-slash');
+        }
+    }
+    </script>
+    @endsection
 </div>
 @endsection
--- resources/views/layouts/page_login.blade.php
+++ resources/views/layouts/page_login.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
@@ -80,6 +81,7 @@
 
         <main class="py-4">
             @yield('content')
+            @yield('scripts')
         </main>
     </div>
 </body>
